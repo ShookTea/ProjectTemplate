961b34ea15705886f02d5c34576b94c9d23a3a82
Norbert Kowalik <kowaliknor@gmail.com>
2020-01-20 19:40:25 +0100

    Update to frontend template

--- templates/frontend/src/router/index.js
+++ templates/frontend/src/router/index.js
@@ -1,17 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
-const routes = [{
-  path: '/',
-  name: 'home',
-  component: () => import('@/views/LoginView.vue'),
-}, {
-  path: '/confirm/:token',
-  name: 'confirm',
-  component: () => import('@/views/ConfirmView.vue'),
-}];
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+];
 
 const router = new VueRouter({
   routes,
--- templates/frontend/src/store/index.js
+++ templates/frontend/src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
 });
