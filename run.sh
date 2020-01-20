58acf0ab3e9ec10b6d5d91da5c413e15678df904
Norbert Kowalik <kowaliknor@gmail.com>
2020-01-20 18:57:58 +0100

    Fixing paths

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@ ALL_TEMPLATE_DIR="/app/templates"
 
 for template in $(find "$ALL_TEMPLATE_DIR"); do
   if [ ! -d "$template" ]; then
-    TEMPLATEPATH=${template#*/*/}
+    TEMPLATEPATH=${template#*/*/*/}
     TEMPLATEDIR=$(dirname $TEMPLATEPATH)
 
     if [ "$TEMPLATEDIR" != "." ]; then
       mkdir -p "$BUILDDIR/$TEMPLATEDIR"
     fi
-    src="$TEMPLATEPATH"
+    src="/app/templates/$TEMPLATEPATH"
     dest="$BUILDDIR/$TEMPLATEPATH"
     echo "Copying '$src' to '$dest'..."
     cat $src | sed -e "s|\${GITLAB_REPO}|${GITLAB_DOCKER_REPO}|" > $dest
