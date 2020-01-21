fde0bd3914de9d97de04dc8ce219e4a61f8b3560
Norbert Kowalik <kowaliknor@gmail.com>
2020-01-21 18:51:20 +0100

    Fixing production image

--- templates/images/entrypoint.sh
+++ templates/images/entrypoint.sh
@@ -1,3 +1,15 @@
-#!/usr/bin/env bash
-service nginx start
-php-fpm
+#!/bin/bash
+# Copyright: Mark Hilton
+# Publish in https://github.com/markhilton/docker-nginx-php-fpm under MIT licence
+
+printf "\n\nStarting PHP 7.0 daemon...\n\n"
+php-fpm --daemonize
+
+printf "Starting Nginx...\n\n"
+set -e
+
+if [[ "$1" == -* ]]; then
+    set -- nginx -g daemon off; "$@"
+fi
+
+exec "$@"
